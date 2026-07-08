8ec391aea03d7892e17382ae88ad9c04039cd63f
Utkarsh <utkarshsrivastava.1392006@gmail.com>
2026-07-09 00:49:38 +0530

    setup

--- app/main.py
+++ app/main.py
@@ -3,3 +3,9 @@ app = FastAPI()
 @app.get("/home")
 def home():
     return "Hello this is home"
+@app.get("/health")
+def health():
+    return {"status":"healthy"}
+@app.get("/about")
+def about():
+    return {"project":"url shortener"}
